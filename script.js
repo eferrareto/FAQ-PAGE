198a4a3961481ed9bc4c7d4c883f934cc3b0c0d9
Erick Ferrareto <e.ferraretowork@gmail.com>
2021-09-03 11:09:47 -0300

    commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function maisinfo(){
     let prim = document.getElementById('primeiro');
     let more = document.createElement('p');
+    more.className = 'paragrafo';
     let lore = document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
     prim.appendChild(more);
     more.appendChild(lore);
@@ -8,6 +9,7 @@ function maisinfo(){
 function maisinfodois(){
     let prim = document.getElementById('segundo');
     let more = document.createElement('p');
+    more.className = 'paragrafo';
     let lore = document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
     prim.appendChild(more);
     more.appendChild(lore);
@@ -15,8 +17,12 @@ function maisinfodois(){
 function maisinfotres(){
     let prim = document.getElementById('terceiro');
     let more = document.createElement('p');
-    prim.className = 'foi'
+    more.className = 'paragrafo';
     let lore = document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
     prim.appendChild(more);
     more.appendChild(lore);
+}
+function menosinfo(){
+    var el = document.getElementsByClassName('paragrafo')[0];
+    el.parentNode.removeChild(el)
 }
